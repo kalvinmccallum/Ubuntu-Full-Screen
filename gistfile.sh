8887af45c877b06d295628967aa5bafc865f1a70
Kalvin McCallum <114109316+kalvinmccallum@users.noreply.github.com>
2023-02-15 10:21:15 -0500

    Create gistfile.sh

---
+++ gistfile.sh
@@ -0,0 +1,3 @@
+sudo apt-get install dkms build-essential linux-headers-generic
+# On virtual machine’s menu bar on top, click on Device/Install Guest Additions. This will launch a unix script runing on the Ubuntu terminal.
+sudo reboot
